99139a8b42f315391b354f66d4375d044a856c15
ImanRS1 <omid-omega@mail.com>
2021-03-05 16:27:16 +0100

    created api fetches for game details and screensht

--- src/api.js
+++ src/api.js
@@ -36,3 +36,7 @@ export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL =  () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${new_games}`;
 
+export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
+
+export const gameScreenshotURL = (game_id) => `${base_url}games/${game_id}/screenshots`;
+
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { findRenderedComponentWithType } from "react-dom/test-utils";
 import {combineReducers} from "redux";
 import gamesReducer from "./gamesReducer";
+import detailReducer from "./detailReducer"
 
 
 const rootReducer = combineReducers({
     games: gamesReducer,
+    detail: detailReducer,
 })
 
 export default rootReducer;
