7f047e1aebf7271dafcdc3f9d2d5b5cba5d89fe8
ImanRS1 <omid-omega@mail.com>
2021-03-08 20:15:14 +0100

    styling of game component,  gameinfo

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,8 +7,8 @@ import { useDispatch } from "react-redux";
 import { loadDetail } from "../actions/detailAction";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
-import {smallImage} from "../util";
-import {popUp} from "../animations";
+import { smallImage } from "../util";
+import { popUp } from "../animations";
 
 const Game = ({ name, released, image, id, metacriticScore }) => {
   const stringPathId = id.toString();
@@ -24,40 +24,85 @@ const Game = ({ name, released, image, id, metacriticScore }) => {
     dispatch(loadDetail(id));
   };
 
-  const checkMetaScore = (mScore) =>{
-    console.log(typeof(mScore));
-    if(typeof(mScore) != "number"){
+  const checkMetaScore = (mScore) => {
+    console.log(typeof mScore);
+    if (typeof mScore != "number") {
       return "N/A";
-    }else{
+    } else {
       return mScore;
     }
-  }
+  };
 
   return (
-    <StyledGame variants={popUp} initial="hidden" animate="show" LayoutId={stringPathId} onClick={loadDetailHandler}>
+    <StyledGame
+      variants={popUp}
+      initial="hidden"
+      animate="show"
+      LayoutId={stringPathId}
+      onClick={loadDetailHandler}
+    >
       <Link to={`/game/${id}`}>
-        <motion.h3>{name}</motion.h3 >
-        <p>{released}</p>
-        <p>{checkMetaScore(metacriticScore)}</p>
+        <StyledGameInfo>
+          <h3>{name}</h3>
+          <p>{released}</p>
+        </StyledGameInfo>
+        <StyledScore>{checkMetaScore(metacriticScore)}</StyledScore>
 
-        <motion.img layoutId={`image ${stringPathId}`} src={smallImage(image, 640)} alt={name} />
+        <motion.img
+          layoutId={`image ${stringPathId}`}
+          src={smallImage(image, 640)}
+          alt={name}
+        />
       </Link>
     </StyledGame>
   );
 };
 
 const StyledGame = styled(motion.div)`
-  min-height: 30vh;
+  height: 40vh;
   box-shadow: 0px 5px 30px rgba(0, 0, 0, 0.2);
-  text-align: center;
+  /*  text-align: center; */
+  color: #fefefe;
   border-radius: 1rem;
   cursor: pointer;
   overflow: hidden;
+  position: relative;
   img {
     width: 100%;
-    height: 40vh;
+    height: 100%;
     object-fit: cover;
   }
+`;
+
+const StyledGameInfo = styled(motion.div)`
+  position: absolute;
+  padding: 1rem;
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  h3 {
+    padding: 0;
+    color: #fefefe;
+  }
+  p {
+    color: #fefefe;
+  }
+`;
+
+const StyledScore = styled(motion.div)`
+  border: 5px solid gold;
+  font-size: 2.5rem;
+  border-bottom-left-radius: 1rem;
+  width: 6rem;
+  height: 5rem;
+  color: white;
+  background-color: #333;
+  position: absolute;
+  font-weight: bold;
+  right: -5px;
+  top: -5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default Game;
